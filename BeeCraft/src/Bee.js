b69577a45b2047ff26a6c5919f0c4c9144576471
StephanyeCunto <stephanyecristine6@gmail.com>
2025-06-09 07:55:48 -0300

    Carregando modelo 3D

--- BeeCraft/src/Bee.js
+++ BeeCraft/src/Bee.js
@@ -1,169 +1,59 @@
 import * as THREE from 'three';
 
-export class Bee{
-    constructor(dimension){
-        this.setupColors();
+export class Bee {
+    let wingLeft = null;
+    let wingRight = null;
+    let beeModel = null;
+    const leg = [];
 
-        this.dimension = dimension;
+    construtor() {
+        const loader = new GLTFLoader();
+        loader.load('/models/bee_minecraft.glb', 
+            (gltf) => {
+                beeModel = gltf.scene;
+                beeModel.position.set(0, 0, 0);
+                beeModel.scale.set(3, 3, 3);
 
-        this.stripeCount = 4;
-        this.stripeDepth =  this.dimension/8;
-        this.stripeDepthCount = this.stripeDepth * this.stripeCount;
+                beeModel.traverse((child) => {
+                    if (child.isMesh) {
+                        child.castShadow = true;
+                        child.receiveShadow = true;
+                    }
+                    
+                    if (child.name === "RingL_4" || child.name === "WingLeft" ) wingLeft = child;
+                    else if (child.name === "RingR_5" || child.name === "WingRight" ) wingRight = child;
+                    else if(child.name?.startsWith("leg")) leg.push(child);
+                });
 
-        this.headDepth =  this.dimension/1.3;
-        this.buttDepth = this.dimension/4;
+                scene.add(beeModel);
+            }
+        );
+    }
 
-        this.antennaDepth = this.dimension/4; 
-        this.antennaWidth = this.dimension/10; 
-        this.antennaHeight = this.dimension/10;
+a {
+    const clock = new THREE.Clock();
 
-        this.stingDepth = this.dimension/4;
-        this.stingWidth = this.dimension/20;
-        this.stingHeight = this.dimension/10;
-
-        this.pawDepth = this.dimension/10;
-        this.pawWidth = this.dimension/10;
-        this.pawHeight = this.dimension/1.5;
-
-        this.eyeDepth = this.headDepth/3;
-        this.eyeWidth = this.dimension/2.5;
-        this.eyeHeight = this.dimension/2.5;
+        const time = clock.getElapsedTime();
+    
+    if (wingLeft && wingRight) {
+      const flapFrequency = 2;
+      const flapAngle = Math.sin(time * flapFrequency) * 0.6; 
         
-        this.group = new THREE.Group();
-
-        this.group.add(this.head(), this.creatStripes(), this.butt(), this.createAntenna(), this.sting(), this.createPaw(), this.createEye());
-    }
-
-    setupColors(){
-        this.yellow = 0xfdd72b;
-        this.black = 0x000000;
-        this.brown = 0x0c0600;
-        this.white = 0xffffff;
-    }
-
-    head(){
-        const geometry = new THREE.BoxGeometry(this.dimension, this.dimension, this.headDepth, 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color : this.yellow});
-        return new THREE.Mesh(geometry,material);
-    }
-
-    creatStripes(){
-        const groupList = new THREE.Group();
-        for(let i = 0; i < this.stripeCount; i++) {
-            (i%2) ? groupList.add(this.stripe(this.yellow,i)) : groupList.add(this.stripe(this.black,i));
-        } 
-        return groupList;
-    }
-
-    stripe(colorList,position){
-        const geometry = new THREE.BoxGeometry(this.dimension, this.dimension, this.stripeDepth, 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color : colorList});
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.z = this.headDepth/2 + (this.stripeDepth * position) + this.stripeDepth/2;
-        return mesh;
-    }
-
-    butt(){
-        const geometry = new THREE.BoxGeometry(this.dimension, this.dimension, this.buttDepth , 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color : this.black});
-        const mesh = new THREE.Mesh(geometry,material);
-        mesh.position.z = this.headDepth/2 + this.stripeDepthCount + this.buttDepth/2;
-        return mesh;
-    }
-
-    createAntenna(){
-        const positionX = this.headDepth/3;
-        const positionY = this.headDepth/2;
-        const positionZ =  -this.headDepth/2 - this.antennaDepth/2;
-        const antennaLowerRigth = this.antenna(true,positionX,positionY, positionZ);
-        const antennaBottomRigth = this.antenna(true,positionX,(positionY + positionY/4), (positionZ+ positionZ/4));
-
-        const antennaLowerLeft = this.antenna(false,positionX,positionY, positionZ);
-        const antennaBottomLeft = this.antenna(false,positionX,(positionY + positionY/4), (positionZ+ positionZ/4));
-
-        const group = new THREE.Group();
-        group.add(antennaLowerRigth,antennaBottomRigth,antennaLowerLeft,antennaBottomLeft);
-
-        return group;
-
-    }
-
-    antenna(side, positionX, positionY, positionZ){
-        const geometry = new THREE.BoxGeometry(this.antennaWidth , this.antennaHeight, this.antennaDepth , 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color : this.black});
-        const mesh = new THREE.Mesh(geometry,material);
-        mesh.position.z = positionZ;
-        mesh.position.y = positionY;
-        (side) ? mesh.position.x = positionX: mesh.position.x = -positionX;
-
-        return mesh;
-    }
-
-    sting(){
-        const geometry = new THREE.BoxGeometry( this.stingWidth, this.stingHeight, this.stingDepth, 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color : this.brown});
-        const mesh = new THREE.Mesh(geometry,material);
-        mesh.position.z = (this.headDepth/2 + this.stripeDepthCount + this.buttDepth) + this.stingDepth/2;
-        return mesh;
-    }
-
-    createPaw(){
-        const group = new THREE.Group();
-        const pawFrontRight = this.paw(true, this.headDepth/10);
-        const pawFrontLeft = this.paw(false, this.headDepth/10);
-
-        const pawMiddleRigth = this.paw(true, this.headDepth/10 + this.headDepth/2);
-        const pawMiddleLeft = this.paw(false, this.headDepth/10 + this.headDepth/2);
+      const phaseOffset = Math.sin(time * 2) * 0.05;
         
+      wingLeft.rotation.z = flapAngle + phaseOffset;
+      wingRight.rotation.z = -flapAngle - phaseOffset;
+        
+      wingLeft.rotation.y = Math.sin(time * flapFrequency * 0.5) * 0.1;
+      wingRight.rotation.y = Math.sin(time * flapFrequency * 0.5) * 0.1;
 
-        const pawLastRigth = this.paw(true, this.headDepth/10 + this.headDepth);
-        const pawLastLeft = this.paw(false, this.headDepth/10 + this.headDepth);
-
-        group.add(pawFrontRight, pawFrontLeft, pawMiddleRigth, pawMiddleLeft, pawLastRigth, pawLastLeft);
-
-        return group;
+      wingLeft.scale.x = -1;
     }
-
-    paw(side, positionZ){
-        const geometry = new THREE.BoxGeometry(this.pawWidth, this.pawHeight, this.pawDepth, 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color : this.brown});
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.y = -this.headDepth/2 - this.pawDepth/2;
-        (side)? mesh.position.x = this.headDepth/2 - this.pawDepth/2 : mesh.position.x =  -this.headDepth/2 + this.pawDepth/2;
-        mesh.position.z = positionZ;
-        return mesh;
+    
+    if (beeModel) {
+        beeModel.position.y = Math.sin(time * 2) * 0.3;
+        beeModel.rotation.x = Math.sin(time * 1.5) * 0.02;
+        beeModel.rotation.z = Math.sin(time * 1.8) * 0.01;
     }
-
-    createEye(){
-        const group = new THREE.Group();
-
-        group.add(this.eye(true),this.eye(false));
-
-        return group;
-    }
-
-    eye(side){
-        const geometry = new THREE.BoxGeometry(this.eyeWidth, this.eyeHeight, this.eyeDepth, 1, 1, 1);
-        const material = new THREE.MeshToonMaterial({color: this.black});
-        const mesh = new THREE.Mesh(geometry, material);
-
-        mesh.position.y = this.headDepth/8 - this.eyeHeight/4;
-        mesh.position.z = -this.headDepth/2 + this.eyeDepth/2.1;
-
-        (side) ? mesh.position.x = this.headDepth/2.5 : mesh.position.x = -this.headDepth/2.5;
-
-
-        const geometryPoitn = new THREE.BoxGeometry(this.eyeWidth/2, this.eyeHeight/2, this.eyeDepth, 1, 1, 1);
-        const materiaPoitn = new THREE.MeshToonMaterial({ color: this.white});
-        const meshPoint = new THREE.Mesh(geometryPoitn,materiaPoitn);
-
-        meshPoint.position.y = this.headDepth/8;
-        meshPoint.position.z = -this.headDepth/2 + this.eyeDepth/2.3;
-
-        (side) ? meshPoint.position.x = this.headDepth/4 : meshPoint.position.x = -this.headDepth/4;
-
-        const group = new THREE.Group();
-        group.add(mesh, meshPoint);
-        return group;
-    }
+}
 }
--- BeeCraft/src/main.js
+++ BeeCraft/src/main.js
@@ -1,34 +1,51 @@
 import * as THREE from 'three';
 import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
-
-import { Bee } from './Bee';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-camera.position.z = 30;
+camera.position.set(0, 10, 30);
 
 const controls = new TrackballControls(camera, renderer.domElement);
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x87ceeb); 
+scene.background = new THREE.Color(0x87ceeb);
 
-const bee = new Bee(8);
-scene.add(bee.group);
-
-function initLights(){
-  const light = new THREE.AmbientLight(0xf6e568, 10);
-  scene.add(light);
+function initLights() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+    scene.add(ambientLight);
+    
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(10, 10, 5);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    scene.add(directionalLight);
 }
 
 function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  renderer.render(scene, camera);  
+    requestAnimationFrame(animate);
+    
+    controls.update();
+    renderer.render(scene, camera);
 }
 
-animate();
+function addGround() {
+    const groundGeometry = new THREE.PlaneGeometry(100, 100);
+    const groundMaterial = new THREE.MeshLambertMaterial({ color: 0x90EE90 });
+    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    ground.rotation.x = -Math.PI / 2;
+    ground.position.y = -10;
+    ground.receiveShadow = true;
+    scene.add(ground);
+}
+
 initLights();
+addGround(); 
+loadBee();
+animate();
